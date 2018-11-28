e6e16993b734997bbf74b8101fffd5e76d34e8fb
Pablo Verdugo Huerta <pablo.verdugo000@gmail.com>
2018-11-28 16:35:12 +0100

    Fork commit

---
+++ src/main/groovy/com/pabloverdugo/imagekit/Utils.groovy
@@ -0,0 +1,39 @@
+package com.pabloverdugo.imagekit
+
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+import java.security.InvalidKeyException
+import java.security.NoSuchAlgorithmException
+
+class Utils {
+
+    private final static String ALGORITHM = "HmacSHA1"
+
+    static String sign(String content, String privateApiKey) {
+        String encoded
+        try {
+            SecretKeySpec signingKey = new SecretKeySpec(privateApiKey.getBytes(), ALGORITHM)
+            Mac mac = Mac.getInstance(ALGORITHM)
+            mac.init(signingKey)
+            encoded = toHexString(mac.doFinal(content.getBytes()))
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Cannot create signature.", e)
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException("Cannot create signature.", e)
+        }
+        return encoded
+    }
+
+    private static String toHexString(byte[] bytes) {
+        Formatter formatter = new Formatter()
+        for (byte b : bytes) {
+            formatter.format("%02x", b)
+        }
+        return formatter.toString()
+    }
+
+    static String timestamp() {
+        return Long.toString(System.currentTimeMillis() / 1000L as long)
+    }
+
+}
